08bbeae7c0256ea85ee07c0118758145c5a2fccc
kamomearm <aymsuq@gmail.com>
2025-03-17 19:05:55 +0900

    Revert "diagrams用にPosの型を(Float, Float)から(Double, Double)に変更"
    This reverts commit 64f7ce5fb0e009551f3caaff6752528bc36d6d41.

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,4 @@
 module Main (main) where
-import Lib
-import Diagrams.Prelude
-import Diagrams.Backend.SVG.CmdLine
 
 main :: IO ()
-main = mainWith myCircle
-
-myCircle :: Diagram B
-myCircle = circle 1
+main = print "A"
