49ff3cf45bb96053fa65fc11107860d732802671
kamomearm <aymsuq@gmail.com>
2025-03-19 23:27:10 +0900

    変数名の変更

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,7 @@ import Lsystem
 -- 置換と式の空白の除去 |> 置換方法と式の取得 |> 関数への変換
 
 main :: IO ()
-main = renderCairo "output.svg" (dims (V2 800 800)) (drawPolyline dragonResult)
+main = renderCairo "output.svg" (dims (V2 800 800)) (drawPolyline result)
 drawPolyline :: [(Double, Double)] -> Diagram B
 drawPolyline points =
     fromVertices (map (p2 . toDouble) points)
@@ -21,16 +21,16 @@ drawPolyline points =
     where
         toDouble (x, y) = (realToFrac x :: Double, realToFrac y :: Double)
 
-dragonDisplaces :: DisplaceRules
-dragonDisplaces = makeDisplacementRules 
+displaces :: DisplaceRules
+displaces = makeDisplacementRules 
     [ 'a' `displace` "a - b"
     , 'b' `displace` "a + b"
     ]
-dragonResult :: [(Double, Double)]
-dragonResult = 
+result :: [(Double, Double)]
+result = 
     getPositions 
         (0, 0) 
         (makeExpr "a") 
         (pi/2) 
-        dragonDisplaces 
+        displaces 
         12
