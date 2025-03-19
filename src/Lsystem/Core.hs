da9e6d07d0ec62fd0975cc23866199ca660d1491
kamomearm <aymsuq@gmail.com>
2025-03-19 21:10:42 +0900

    DisplaceRulesのコンストラクタの作成

--- src/Lsystem/Core.hs
+++ src/Lsystem/Core.hs
@@ -1,15 +1,22 @@
-module Lsystem.Core 
+module Lsystem.Core
     ( Identifer
     , makeIdent
     , Expression
     , makeExpr
+    , DisplaceRules
+    , makeDisplacementRules
     ) where
-        
-import Data.Char (isAlpha)
 
+import Data.Char (isAlpha)
+import qualified Data.Map.Strict as MS
 
 
 newtype Identifer = Identifer Char
+instance Eq Identifer where
+    (Identifer x) == (Identifer y) = x == y
+instance Ord Identifer where
+    (Identifer x) <= (Identifer y) = x <= y
+    
 makeIdent :: Char -> Identifer
 makeIdent c
     | isAlpha c = Identifer c
@@ -20,3 +27,8 @@ makeExpr :: String -> Expression
 makeExpr ss
     | all (\c -> isAlpha c || c == '+' || c == '-') ss = Expression ss
     | otherwise = error "無効な記号が式に含まれています。式中に使える記号はアルファベットと+/-のみです"
+
+newtype DisplaceRules = DR (MS.Map Identifer Expression)
+-- dragonTikan = DisPlacementRule ['a' `displace` "a - b",  'b' `displace` "a + b" ]
+makeDisplacementRules :: [(Identifer, Expression)] -> DisplaceRules
+makeDisplacementRules xs = DR (MS.fromList xs)
