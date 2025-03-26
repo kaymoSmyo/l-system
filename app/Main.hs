220f4ddf5046219f0a2971bdb64945e9f44be7f7
kaymoSmyo <aymsuq@gmail.com>
2025-03-27 01:43:36 +0900

    close #4

--- app/Main.hs
+++ app/Main.hs
@@ -3,10 +3,7 @@ module Main (main) where
 import Diagrams.Prelude
 -- import Diagrams.Backend.SVG.CmdLine
 import Diagrams.Backend.Cairo
-
 import Lsystem
--- 手順
--- 置換と式の空白の除去 |> 置換方法と式の取得 |> 関数への変換
 
 main :: IO ()
 main = renderCairo "output.svg" (dims (V2 800 800)) (drawPolyline result)
@@ -21,16 +18,6 @@ drawPolyline points =
     where
         toDouble (x, y) = (realToFrac x :: Double, realToFrac y :: Double)
 
-displaces :: DisplaceRules
-displaces = makeDisplacementRules 
-    [ 'a' `displace` "a+b++b-a--aa-b+"
-    , 'b' `displace` "-a+bb++b+a--a-b"
-    ]
+drs = makeDRs ["A -> A - B", "B -> A + B"]
 result :: [(Double, Double)]
-result = 
-    getPositions 
-        (0, 0) 
-        (makeExpr "a") 
-        (pi/3) 
-        displaces 
-        5
+result = getPoss drs (pi/2) 12 "A" (0, 0)
--- src/Lsystem.hs
+++ src/Lsystem.hs
@@ -1,17 +1,5 @@
 module Lsystem 
-    ( 
+    ( makeDRs
+    , getPoss
     ) where
-import Lsystem.Parser
 import Lsystem.Core
-import Lsystem.Types
-import qualified Data.Map.Strict as MS
-
-
-makeDRs :: [String] -> DRs
-makeDRs ss = MS.fromList rules
-    where
-        rules = do
-            s <- ss
-            case runParser parseDisplaceRule s of
-                [(dr, _)] -> [dr] -- 最初の結果のみ使用
-                _ -> error "faild parse"        -- パース失敗
