b1563fc5ba0ee47e7a801e60fefd2da689ca0cb3
kaymoSmyo <aymsuq@gmail.com>
2025-04-17 18:09:15 +0900

    アプリケーション層とのやり取りの作成

---
+++ src/Lsystem/InOut.hs
@@ -0,0 +1,22 @@
+module Lsystem.InOut
+    ( evalLsystem
+    , execLsystem
+    ) where
+
+import Lsystem.EvalExpr
+import Lsystem.Parser
+import Lsystem.Types
+
+
+evalLsystem :: String -> [String] -> Int -> Either ParseError Expression
+evalLsystem a ss n = do
+    axiom <- fst <$> runParser parseExpression a
+    rules <- mapM parseRule ss
+    let drs = fromList rules
+    pure $ nExpansion axiom drs n
+  where
+    parseRule :: String -> Either ParseError DisplaceRule
+    parseRule s = fst <$> runParser parseDisplaceRule s
+
+execLsystem :: Expression -> Angle -> Pos -> [Pos]
+execLsystem = getPoss 
